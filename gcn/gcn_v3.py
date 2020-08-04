5aa10a800130b1d63ee2d3e47bb797094f572bef
yuwanlong <yuwanlong@dayuwuxian.com>
2020-08-04 16:31:19 +0800

    add gcn

--- gcn/gcn_v3.py
+++ gcn/gcn_v3.py
@@ -1,7 +1,7 @@
 """GCN using DGL nn package
 References:
 - Author: yuwanlong gzyuwanlong@163.com
-- Graph Convolutional Networks version one
+- Graph Convolutional Networks version three
 - Paper: https://arxiv.org/abs/1609.02907
 - Code: https://github.com/tkipf/gcn
 """
@@ -15,42 +15,9 @@ import torch.nn as nn
 from dgl import DGLGraph
 import networkx as nx
 import torch.nn.functional as F
+import dgl.function as fn
 import utils
 from dgl.data import register_data_args
-
-
-def gcn_msg(edge):
-    msg = edge.src['h'] * edge.src['norm']
-    return {'m': msg}
-
-
-def gcn_reduce(node):
-    accum = torch.sum(node.mailbox['m'], dim=1) * node.data['norm']
-    return {'h': accum}
-
-
-class NodeApplyModule(nn.Module):
-    def __init__(self, out_feats, activation=None, bias=True):
-        super(NodeApplyModule, self).__init__()
-        if bias:
-            self.bias = nn.Parameter(torch.Tensor(out_feats))
-        else:
-            self.bias = None
-        self.activation = activation
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        if self.bias is not None:
-            stdv = 1. / math.sqrt(self.bias.size(0))
-            self.bias.data.uniform_(-stdv, stdv)
-
-    def forward(self, nodes):
-        h = nodes.data['h']
-        if self.bias is not None:
-            h = h + self.bias
-        if self.activation:
-            h = self.activation(h)
-        return {'h': h}
 
 
 class GCNLayer(nn.Module):
@@ -58,23 +25,36 @@ class GCNLayer(nn.Module):
         super(GCNLayer, self).__init__()
         self.g = g
         self.weight = nn.Parameter(torch.Tensor(in_feats, out_feats))
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(out_feats))
+        else:
+            bias = None
+        self.activation = activation
         if dropout:
             self.dropout = nn.Dropout(p=dropout)
         else:
             self.dropout = 0.
-        self.node_update = NodeApplyModule(out_feats, activation, bias)
         self.reset_parameters()
-
+        
     def reset_parameters(self):
         stdv = 1. / math.sqrt(self.weight.size(1))
         self.weight.data.uniform_(-stdv, stdv)
-
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+    
     def forward(self, h):
         if self.dropout:
             h = self.dropout(h)
-        self.g.ndata['h'] = torch.mm(h, self.weight)
-        self.g.update_all(gcn_msg, gcn_reduce, self.node_update)
+        h = torch.mm(h, self.weight)
+        h = h * self.g.ndata['norm']
+        self.g.ndata['h'] = h
+        self.g.update_all(fn.copy_src(src='h', out='m'), fn.sum(msg='m', out='h'))
         h = self.g.ndata.pop('h')
+        h = h * self.g.ndata['norm']
+        if self.bias is not None:
+            h = h + self.bias
+        if self.activation:
+            h = self.activation(h)
         return h
 
 
@@ -82,7 +62,7 @@ class GCN(nn.Module):
     def __init__(self, g, in_feats, n_hidden, n_classes, n_layers, activation, dropout):
         super(GCN, self).__init__()
         self.layers = nn.ModuleList()
-        self.layers.append(GCNLayer(g, in_feats, n_hidden, activation, dropout))
+        self.layers.append(GCNLayer(g, in_feats, n_hidden, activation, 0.))
         for _ in range(n_layers - 1):
             self.layers.append(GCNLayer(g, n_hidden, n_hidden, activation, dropout))
         self.layers.append(GCNLayer(g, n_hidden, n_classes, None, dropout))
@@ -187,3 +167,5 @@ if __name__ == "__main__":
     print(args)
 
     main(args)
+
+
